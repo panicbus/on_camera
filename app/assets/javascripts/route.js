49daa8426e43cb4cafbf46970f200f59796f1263
panicbus <elementalair@gmail.com>
2014-05-22 19:09:32 -0700

    added all station, route coords

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -1,51 +1,213 @@
 // all the json data for marker locations and routeline
 
+
+// var markerIcon = {
+//   url: '/assets/cctv_marker.png'
+// };
+
+
 var geoJson_features = [
 {
   type: "Feature",
   geometry: {
     type: "Point",
-    coordinates: [-118.44536304473877, 34.06382338375145]
+    coordinates: [-118.15211176872253, 34.14567708986222]
   },
   properties: {
     title: 'Camera 1',
-    description: 'Westwood Village',
-    'marker-color': '#548cba',
+    description: 'Downtown Pasadena',
+    'marker-color': '#2b2b2b',
     'marker-size': 'large',
     'marker-symbol': 1,
+    // icon: markerIcon,
     location_id: 1
   }
 },
-
 {
   type: "Feature",
   geometry: {
     type: "Point",
-    coordinates: [-122.39996910095215, 37.79325327800921]
+    coordinates: [-118.23827505111693, 34.04916593685887]
   },
   properties: {
-    title: 'Station 9',
-    description: 'Financial District',
-    'marker-color': '#548cba',
+    title: 'Camera 2',
+    description: 'Convention Center',
+    'marker-color': '#2b2b2b',
     'marker-size': 'large',
-    'marker-symbol': 9,
-    location_id: 9
+    'marker-symbol': 2,
+    location_id: 2
+  }
+},
+{
+  type: "Feature",
+  geometry: {
+    type: "Point",
+    coordinates: [-118.23845744132996, 34.06614309903811]
+  },
+  properties: {
+    title: 'Camera 3',
+    description: 'Chinatown',
+    'marker-color': '#2b2b2b',
+    'marker-size': 'large',
+    'marker-symbol': 3,
+    location_id: 3
+  }
+},
+{
+  type: "Feature",
+  geometry: {
+    type: "Point",
+    coordinates: [-118.34167420864105, 34.10160638993377]
+  },
+  properties: {
+    title: 'Camera 4',
+    description: 'Hollywood Boulevard',
+    'marker-color': '#2b2b2b',
+    'marker-size': 'large',
+    'marker-symbol': 4,
+    location_id: 4
   }
 },
 
+
+
 ////// the route linestring starts here ///////
-{
-  "geometry": {
-    "coordinates": [
-      [
-      -122.42674827575682,
-      37.76786513360663
-    ],
+// {
+//   "geometry": {
+//     "coordinates": [
+//       [
+//       -122.42674827575682,
+//       37.76786513360663
+//     ],
     
-    [ -122.42674827575682,
-      37.76786513360663
-    ]
-  ],
+//     [ -122.42674827575682,
+//       37.76786513360663
+//     ]
+//   ],
+//           "type": "LineString"
+//         },
+//       "properties": {
+//         "stroke": "#000",
+//         "stroke-opacity": 1,
+//         "stroke-width": 3
+//       },
+//        type: "Feature"
+//     }
+// ];
+
+/// LA route from geojson.io
+{
+  "geometry":{
+    "coordinates":[
+          [
+            -118.23816776275635,
+            34.04938817298568
+          ],
+          [
+            -118.23808193206789,
+            34.051201598010444
+          ],
+          [
+            -118.24447631835936,
+            34.05657053094319
+          ],
+          [
+            -118.24220180511475,
+            34.058028261980844
+          ],
+          [
+            -118.24108600616455,
+            34.05923709309435
+          ],
+          [
+            -118.23803901672365,
+            34.06599200840498
+          ],
+          [
+            -118.23855400085448,
+            34.066134211361266
+          ],
+          [
+            -118.23812484741211,
+            34.065778703523065
+          ],
+          [
+            -118.23872566223146,
+            34.064463311552615
+          ],
+          [
+            -118.24306011199951,
+            34.065956457628594
+          ],
+          [
+            -118.24117183685301,
+            34.06670302079997
+          ],
+          [
+            -118.2409143447876,
+            34.06709407507407
+          ],
+          [
+            -118.24267387390138,
+            34.06805392791322
+          ],
+          [
+            -118.24717998504637,
+            34.072462001305944
+          ],
+          [
+            -118.25104236602782,
+            34.072568645401255
+          ],
+          [
+            -118.25228691101073,
+            34.074559310537
+          ],
+          [
+            -118.26069831848145,
+            34.07814949902613
+          ],
+          [
+            -118.26520442962646,
+            34.077403036722764
+          ],
+          [
+            -118.26657772064209,
+            34.0783272271763
+          ],
+          [
+            -118.26988220214844,
+            34.07996230865873
+          ],
+          [
+            -118.27151298522948,
+            34.08170399116713
+          ],
+          [
+            -118.2738733291626,
+            34.083303463958096
+          ],
+          [
+            -118.27730655670165,
+            34.09026970418749
+          ],
+          [
+            -118.27889442443848,
+            34.09098051279948
+          ],
+          [
+            -118.28597545623778,
+            34.09744859688645
+          ],
+          [
+            -118.29279899597168,
+            34.101819604583554
+          ],
+          [
+            -118.3417224884033,
+            34.10160638993377
+          ]
+         ],
           "type": "LineString"
         },
       "properties": {
@@ -56,3 +218,4 @@ var geoJson_features = [
        type: "Feature"
     }
 ];
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-locations = [ ['Station 1', 'City Hall', -122.41713523864746, 37.77956816727315],
-							['Station 2', 'Chinatown Gate', -122.40524768829346, 37.78875968591083],
-							['Station 3', 'Entrance to the Presidio on Lombard', -122.44736909866332, 37.79850958608104],
-							['Station 4', 'Lincoln Avenue and the Great Highway', -122.51024007797241, 37.76408245273552],
-							['Station 5', 'AIDS Memorial, Golden Gate Park', -122.4603670835495, 37.76968431368863],
-							['Station 6', 'Laguna Honda and Dewey, backside of Twin Peaks', -122.4591064453125, 37.74728704926913],
-							['Station 7', 'Cesar Chavez and Dolores', -122.42473125457762, 37.747931791686426],
-							['Station 8', 'Embarcero and the Ferry Building', -122.39400386810303, 37.795220198366735],
-							['Station 9', 'Financial District', -122.39996910095215, 37.79325327800921] ] 
+locations = [ ['Camera 1', 'Pasadena downtown', -118.15211176872253, 34.14567708986222],
+							['Camera 2', 'Convention Center', -118.23827505111693, 34.04916593685887],
+							['Camera 3', 'Chinatown',  -118.23845744132996, 34.06614309903811],
+							['Camera 4', 'Hollywood Boulevard', -118.34167420864105, 34.10160638993377], 
 
 locations.each do |location|
 	Location.create(name:location[0], description:location[1], lat:location[2], long:location[3])
